77dbe0c27f0f4b1d0e8e69f5aaa8f62a51d089ce
binhex <megalith01@gmail.com>
2016-04-09 21:45:00 +0100

    tweak scheduler to 10 mins, fix up curl regex

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -29,10 +29,13 @@ echo "; Specify user listener group" >> /etc/php/php-fpm.conf
 echo "listen.group = users" >> /etc/php/php-fpm.conf
 
 # set path to curl as rutorrent doesnt seem to find it on the path statement
-sed -i -e "s~\"curl\".*\=>.*\,~\"curl\"  \=> \'/usr/bin/curl\'\,~g" "/etc/webapps/rutorrent/conf/config.php"
+sed -i -e "s~\"php\"[[:space:]]\+\=>[[:space:]]\+'',~\"php\"   \=> \'/usr/bin/curl\'\,~g" "/etc/webapps/rutorrent/conf/config.php"
 
 # set the rutorrent autotools/autowatch plugin to 30 secs scan time, default is 300 secs
 sed -i -e "s~\$autowatch_interval \= 300\;~\$autowatch_interval \= 30\;~g" "/usr/share/webapps/rutorrent/plugins/autotools/conf.php"
+
+# set the rutorrent schedulder plugin to 10 mins, default is 60 mins
+sed -i -e "s~\$updateInterval \= 60\;~\$updateInterval \= 10\;~g" "/usr/share/webapps/rutorrent/plugins/scheduler/conf.php"
 
 # set the rutorrent diskspace plugin to point at the /data volume mapping, default is /
 sed -i -e "s~\$partitionDirectory \= \&\$topDirectory\;~\$partitionDirectory \= "/data/";~g" "/usr/share/webapps/rutorrent/plugins/diskspace/conf.php"
