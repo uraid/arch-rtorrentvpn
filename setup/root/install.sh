0807142473f8420d3e58684c4a7e144da511e983
uraid <uraid@users.noreply.github.com>
2017-01-28 22:58:14 +0200

    Added ffmpeg to support screenshots plugin

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -19,7 +19,7 @@ find /tmp/scripts-master/ -type f -name '*.sh' -exec mv -i {} /root/  \;
 ####
 
 # define pacman packages
-pacman_packages="git nginx php-fpm rsync openssl tmux gnu-netcat mediainfo npm nodejs php-geoip"
+pacman_packages="git nginx php-fpm rsync openssl tmux gnu-netcat mediainfo npm nodejs php-geoip ffmpeg"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
